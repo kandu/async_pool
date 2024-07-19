70f59b33c929292f11a001ae4e4e8bdafaabbded
ZAN DoYe <zandoye@gmail.com>
2024-07-19 20:57:15 +0800

    add test cases

--- test/async_pool_test.ml
+++ test/async_pool_test.ml
@@ -1,30 +1,56 @@
 open Core
 open Async
-open Async_unix
 
-let pool= Async_pool.create 2 (fun ()-> return ())
+let%test_module _= (module struct
 
+let counter= ref 0
 
-let stdout= Lazy.force Writer.stdout
+let pool= Async_pool.create
+  ~idle_limit:(Time.Span.of_sec 1.)
+  3
+  (fun ()->
+    counter:= !counter+1;
+    Writer.writef (Lazy.force Writer.stdout) "%d opened\n" !counter;
+    return !counter)
 
-let p n=
-  let c= ref 0 in
-  let rec p n=
-    c:= !c + 1;
-    if !c > n * 4 then
-      return ()
-    else
-      (Writer.writef stdout "%d\n" n;
-      after (sec 1.) >>= fun ()->
-      p n)
-  in
-  p n
+let run v=
+  let stdout= (Lazy.force Writer.stdout) in
+  let%bind ()= Clock.after (sec 2.) in
+  Writer.writef stdout "%d\n" v;
+  Writer.flushed stdout
+
+let new_thread hi f=
+  Async_pool.use hi f
 
 let main ()=
-  don't_wait_for @@ Async_pool.use pool (fun ()-> p 1);
-  don't_wait_for @@ Async_pool.use pool (fun ()-> p 2);
-  don't_wait_for @@ Async_pool.use pool (fun ()-> p 3);
-  Async_pool.use pool (fun ()-> p 4)
+  don't_wait_for @@ new_thread pool run;
+  don't_wait_for @@ new_thread pool run;
+  Clock.after (sec 1.9) >>= fun ()->
+  don't_wait_for @@ new_thread pool run; (* count is 2 and capacity is 3, so resource 3 is created here *)
+  don't_wait_for @@ new_thread pool run; (* wait for 0.1 sec, and resource 1 is released, this sentence acquires the resource *)
+  Clock.after (sec 2.2) >>= fun ()-> (* resources 1, 2, 3 are idle, but only resource 2 idles for more then 1 second, so is released *)
+  don't_wait_for @@ new_thread pool run; (* resource 2 is disposed, so new resource 4 is created *)
+  don't_wait_for @@ new_thread pool run;
+  don't_wait_for @@ new_thread pool run;
+  don't_wait_for @@ new_thread pool run;
+  don't_wait_for @@ new_thread pool run;
+  Clock.after (sec 6.)
 
-let ()= Thread_safe.block_on_async_exn main
+let%expect_test "Async_pool"=
+  Thread_safe.block_on_async_exn main;
+  [%expect {|
+    1 opened
+    2 opened
+    3 opened
+    1
+    2
+    3
+    1
+    4 opened
+    3
+    1
+    4
+    3
+    1 |}]
 
+end)
